069ee60d740fb6db4e87be00274f9c1d7ec67eee
Udo Eisenbarth <u.eisenbarth@gsi.de>
2011-01-19 13:50:24 +0100

    initial generation of rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PSDB::Application.config.secret_token = 'd42a897c02fe257568726e045fb951a6dddd572d2f9fe1a8c801df6aac92e24b724005fccfd6275ddfe15c530dedfee18d9dbff04457881fa697ec5de5ee4359'
