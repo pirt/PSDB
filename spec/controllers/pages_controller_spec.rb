0144514bd164edb0cc61fcd6df5f1fe60dbf47eb
Udo Eisenbarth <u.eisenbarth@gsi.de>
2011-01-20 18:07:54 +0100

    added further tests to Pages controller

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+        :content => "PHELIX shot database")
     end
   end
 
@@ -13,6 +19,11 @@ describe PagesController do
     it "should be successful" do
       get 'about'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+        :content => "PHELIX shot database")
     end
   end
 end
