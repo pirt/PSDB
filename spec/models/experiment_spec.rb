bc57e2a7bd70734a523be9060969a1babe0cffc4
Udo Eisenbarth <u.eisenbarth@gsi.de>
2011-01-21 15:05:46 +0100

    added further tests to Experiment model.

--- spec/models/experiment_spec.rb
+++ spec/models/experiment_spec.rb
@@ -5,19 +5,39 @@ describe Experiment do
   before(:each) do
     @attr = { :name => "P0038", :description => "Experiment description" }
   end
+  
+  it "should create a new instance given valid attributes" do
+    Experiment.create!(@attr)
+  end
 
   it "should require a name" do
     no_name_experiment = Experiment.new(@attr.merge(:name => ""))
     no_name_experiment.should_not be_valid
   end
-  it "should have a unique name"
-  it "should reject names that are too long"
+
+  it "should have a unique name" do
+    Experiment.create!(@attr)
+    experiment_with_duplicate_name = Experiment.new(@attr.merge(:description => "Another description"))
+    experiment_with_duplicate_name.should_not be_valid
+
+  end
+
+  it "should reject names that are longer than 30 characters" do
+    long_name = "a" * 31
+    long_name_experiment = Experiment.new(@attr.merge(:name => long_name))
+    long_name_experiment.should_not be_valid
+  end
 
   it "should require a description" do
     no_description_experiment = Experiment.new(@attr.merge(:description => ""))
     no_description_experiment.should_not be_valid
   end
-  it "should reject descriptions that are too long"
+
+  it "should reject descriptions that are longer than 255 characters" do
+   long_description = "a" * 256
+   long_description_experiment = Experiment.new(@attr.merge(:name => long_description))
+   long_description_experiment.should_not be_valid
+  end
 
   it "cannot be deleted if still referenced by shots"
 end
