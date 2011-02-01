ab5ca33ad32ea021f53458052e454eaa053bf842
Udo Eisenbarth <u.eisenbarth@gsi.de>
2011-02-01 16:26:41 +0100

    aded "new" action to attachments controller and a corresponding (preliminary) view for it

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,2 +1,16 @@
 class AttachmentsController < ApplicationController
+  def show
+  end
+  def new
+    @attachment=Attachment.new
+    @pageTitle="Add attachment"
+  end
+  def create
+  end
+  def edit
+  end
+  def update
+  end
+  def destroy
+  end
 end
