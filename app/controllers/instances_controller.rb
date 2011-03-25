6e0289d72858ecf2b455978dc8c6519be11a6408
Udo Eisenbarth <udo.eisenbarth@arcor.de>
2011-03-25 19:27:31 +0100

    added "alpha" to PHELIX logo. implemented filtering by subsystem or classtype in instances#index.

--- app/controllers/instances_controller.rb
+++ app/controllers/instances_controller.rb
@@ -1,6 +1,15 @@
 class InstancesController < ApplicationController
   def index
-    @instances=Instance.order("name ASC").paginate(:page => params[:page])
+    if (params[:subsystemName])
+      selectedSubsystem=Subsystem.find_by_name(params[:subsystemName])
+      selectedInstances=selectedSubsystem.instances.order("name ASC")
+    elsif (params[:classtypeName])
+      selectedClasstype=Classtype.find_by_name(params[:classtypeName])
+      selectedInstances=selectedClasstype.instances.order("name ASC")
+    else
+      selectedInstances=Instance.order("name ASC")
+    end
+    @instances=selectedInstances.includes(:classtype, :subsystem).paginate(:page => params[:page])
     @availableClasstypes=Classtype.all
     @availableSubsystems=Subsystem.all
   end
