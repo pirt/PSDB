28adbe9f924717f93d016258920fbf6e12b2492f
Udo Eisenbarth <u.eisenbarth@gsi.de>
2011-06-10 19:52:35 +0200

    added display of git repository version information in page footer.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 PSDB::Application.initialize!
+
+# define version string from git repository
+APP_VERSION = `git describe --always` unless defined? APP_VERSION
+
