adfe7ad1bcc74b91c6eb6c580d1ff16264cc4ad9
Udo Eisenbarth <u.eisenbarth@gsi.de>
2011-06-10 19:31:19 +0200

    added display of view availability for a given instance and version number.

--- app/helpers/instances_helper.rb
+++ app/helpers/instances_helper.rb
@@ -1,2 +1,7 @@
 module InstancesHelper
+  def viewExists(classType,version,detailed=false)
+    viewType= detailed ? "/_detailed_" : "/_short_"
+    viewFileName=::Rails.root.to_s+"/app/views/instancevaluesets/"+classType+viewType+version.to_s+".html.erb"
+    return File.exists?(viewFileName)
+  end
 end
