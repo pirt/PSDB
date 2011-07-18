cd2e8a230af568d865d8cba61473056fdfd2a294
Udo Eisenbarth <u.eisenbarth@gsi.de>
2011-07-18 17:37:52 +0200

    Handle missing psdbconfig.yml file during startup and simplified startup scripts.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,17 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Load PSDB_CONFIG
+begin
+  raw_config = File.read(Rails.root.to_s + "/config/psdbconfig.yml")
+  PSDB_CONFIG = YAML.load(raw_config)
+rescue
+  puts
+  puts "Error while trying to read config/psdbconfig.yml"
+  puts "Try to run 'ruby lib/psdbConfig.rb' first and follow the instructions there."
+  exit!
+end
+
 # Initialize the rails application
 PSDB::Application.initialize!
 
--- config/initializers/psdb_config.rb
+++
@@ -1,3 +0,0 @@
-raw_config = File.read(RAILS_ROOT + "/config/psdbconfig.yml")
-PSBDB_CONFIG = YAML.load(raw_config)
-
